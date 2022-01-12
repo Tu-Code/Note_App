5197dc2e7bf1d3f6e776be5e4ffe1e3830fdc421
Tu-Code <83554169+Tu-Code@users.noreply.github.com>
2022-01-12 13:25:45 +0100

    Forgot Password

--- OneDrive/Desktop/Note_App-main/website/__init__.py
+++ OneDrive/Desktop/Note_App-main/website/__init__.py
@@ -1,10 +1,9 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_mail import Mail
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 migrate = Migrate()
@@ -14,15 +13,9 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Jesus'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    # app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-    # app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-   
     db.init_app(app)
     migrate.init_app(app, db)
-    mail = Mail(app)
+
     
     from .views import views
     from .auth import auth
