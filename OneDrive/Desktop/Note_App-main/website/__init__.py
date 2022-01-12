be092756246e4817cca44d4c287b6026d2a1c479
Tu-Code <83554169+Tu-Code@users.noreply.github.com>
2022-01-12 13:13:23 +0100

    Forget password functionality

---
+++ OneDrive/Desktop/Note_App-main/website/__init__.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_mail import Mail
+db = SQLAlchemy()
+DB_NAME = "database.db"
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'Jesus'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    # app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+    # app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+   
+    db.init_app(app)
+    migrate.init_app(app, db)
+    mail = Mail(app)
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+        
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
