c4b76dcbe80b76ddf6c1e4b744f3f51b5284e740
Tu-Code <83554169+Tu-Code@users.noreply.github.com>
2022-01-20 19:37:57 +0100

    Changed css, js, added venv

--- website/static/index.js
+++ website/static/index.js
@@ -1,23 +1,19 @@
 
-function deleteNote(noteId){
-    //this will take the noteId, send post request to del note endpoint
-    fetch('/delete-note',{
-        method: 'POST',
-        body: JSON.stringify({ noteId: noteId})
-    }).then((_res) =>{
-        //and then reload window nd redirect to home page
-        window.location.href = '/';
-    })
-}
-// let i = 0;
+
+const deleteNote = id=> fetch(`/delete-note`,{
+    method: 'DELETE',
+    body: JSON.stringify({ noteId: id})
+}).then((_res) =>{
+    //and then reload window nd redirect to home page
+    // let x = activity_id +  1;
+    $(`.${id}`).remove();
+    // document.getElementById('{{note.id}}').style.display = 'none';  
+    
+    $(`#app-${id}`).remove();
+    alert("deleted " + id)
+})
+
 let onAddNote = () =>{
     document.getElementById('onAddNote').style.display = 'block';  
     document.getElementById('notes').style.display = 'none';  
 }
-// let arrayOfNotes = []
-
-// 'clickedNote({{ note.title, note.data }})
-// document.getElementById("app").addEventListener("click", function() {
-//     document.getElementById("noteData").innerHTML = this.data;
-//     // document.getElementById('notes').style.display = 'block';
-//   });
--- website/views.py
+++ website/views.py
@@ -4,9 +4,14 @@ from flask_login import  login_required, current_user
 from werkzeug.wrappers import request
 from flask import flash, request, jsonify
 from .models import Note
+from .forms import RequestResetForm, ResetPasswordForm
 from . import db
+from flask_mail import Message
 import json
-
+#forgot pwd
+# from .forms import EmailForm
+from .models import User
+# from .util import send_email, ts
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
@@ -22,13 +27,13 @@ def home():
             new_note = Note(title=Title, data=note, user_id = current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
+            # flash('Note added!', category='success')
             # return render_template('views.html', data=)
     
     return render_template('home.html', user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
+@views.route('/delete-note', methods=['DELETE'])
 def delete_note():
     note = json.loads(request.data)
     noteId = note['noteId']
@@ -37,12 +42,66 @@ def delete_note():
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
-    return jsonify({})   
+    return jsonify({})
 
-# @views.route('/add-to-entry', methods=['POST'])
-# def add_to_Entry():
-#     noteD = json.loads(request.data)
-#     noteData = noteD['noteData']
-#     noteD = Note.query.get(noteData)
-#     if noteD:
-#         if note.
+def send_reset_email(user):
+    from . import mail
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='solarinsarah3@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+    {url_for('reset_token', token=token, _external=True)}
+    If you did not make this request then simply ignore this email and no changes will be made.
+    '''
+    mail.send(msg)
+
+@views.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+@views.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('Invalid or expired token', 'warning')
+        return redirect(url_for('reser_request'))
+    form = ResetPasswordForm()
+    if request.method == 'POST':
+        # email = request.form.get('email')
+        # first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        # user = User.query.filter_by(email=email).first()
+        # if user:
+        #     flash('Email already exists.', category='error')
+        # if len(email) < 4:
+        #     flash('Email must be greater than 3 characters', category='error')
+        # elif len(first_name) < 2:
+        #     flash('Name must be greater than 1 character', category='error')
+        if password1 != password2:
+            flash('Password\'s dont match', category='error')
+        elif len(password1) < 7:
+            flash('Password is too short', category='error')
+        else:
+            # account created
+            user.password = password1
+            db.session.commit()
+            # login_user(user)
+            flash('Password updated!', category='success')
+            # going back to views -> home
+            return redirect(url_for('auth.login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
+
+
